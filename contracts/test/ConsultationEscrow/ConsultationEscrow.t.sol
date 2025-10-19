9e3f3b9206c748e606f092a977ab78174e176f15
plairfx <plairfx@gmail.com>
2025-10-19 23:45:34 +0200

    feat: added test features

--- contracts/test/ConsultationEscrow/ConsultationEscrow.t.sol
+++ contracts/test/ConsultationEscrow/ConsultationEscrow.t.sol
@@ -18,12 +18,51 @@ contract ConsultationEscrowTest is Test {
     address doctor = makeAddr("doctor");
     address alice = makeAddr("alice");
 
+    uint256 consultationFeePerHour = 50e6;
+    uint256 depositFee = 10e6;
+
     function setUp() public {
+        vm.startPrank(admin);
         PYUSD = new MockPyUSD();
         Pyth = new MockPyth();
-        DocReg = new DoctorRegistry(admin, 10e6, address(PYUSD));
+        DocReg = new DoctorRegistry(admin, depositFee, address(PYUSD));
         ConsultEscrow = new ConsultationEscrow(address(DocReg), address(Pyth), address(PYUSD));
+
+        vm.stopPrank();
     }
 
-    function test_createSession() public {}
+    modifier doctorCreatedAndApproved() {
+        vm.startPrank(alice);
+        PYUSD.faucet(10e6);
+
+        Structs.RegStruct memory Reg =
+            Structs.RegStruct("Alice", "Mental-Health-Therapy", alice, consultationFeePerHour, 0);
+
+        PYUSD.approve(address(DocReg), depositFee);
+        DocReg.registerAsDoctor(Reg);
+
+        vm.startPrank(admin);
+        DocReg.grantRole(keccak256("APPROVER"), admin);
+        DocReg.approveDoctor(1);
+        _;
+    }
+
+    function test_createSession() public doctorCreatedAndApproved {
+        // needs data..
+        bytes[] memory barray = new bytes[](1);
+        vm.startPrank(alice);
+
+        vm.expectRevert("Doctor not found");
+        ConsultEscrow.createSession(2, consultationFeePerHour, barray);
+
+        vm.expectRevert("Insufficient contract ETH for oracle fee");
+        ConsultEscrow.createSession(1, consultationFeePerHour, barray);
+
+        vm.deal(address(ConsultEscrow), 1 ether);
+
+        //@info: getEthToUsd func does not exist,(reverts atm).
+
+        // vm.expectRevert("Insufficient USD value from ETH");
+        // ConsultEscrow.createSession(1, consultationFeePerHour, barray);
+    }
 }
